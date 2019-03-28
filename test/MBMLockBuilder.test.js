7d7b4a8e77b51d9aaa42c064af393a4f4423c717
vittominacori <vittoriominacori@gmail.com>
2019-03-28 19:36:07 +0100

    improving tests

--- test/MBMLockBuilder.test.js
+++ test/MBMLockBuilder.test.js
@@ -9,7 +9,7 @@ const LockBuilder = artifacts.require('MBMLockBuilder');
 const TokenTimelock = artifacts.require('MBMTimelock');
 
 contract('MBMLockBuilder', function ([owner, beneficiary, thirdParty]) {
-  it('rejects with zero address', async function () {
+  it('rejects with zero address as token', async function () {
     await shouldFail.reverting(
       LockBuilder.new(ZERO_ADDRESS)
     );
@@ -32,40 +32,53 @@ contract('MBMLockBuilder', function ([owner, beneficiary, thirdParty]) {
       });
 
       context('try creating a timelock', function () {
-        beforeEach(async function () {
-          this.releaseTime = (await time.latest()).add(time.duration.years(1));
+        describe('if thirdParty is calling', function () {
+          it('reverts', async function () {
+            const futureReleaseTime = (await time.latest()).add(time.duration.years(1));
+
+            await shouldFail.reverting(
+              this.builder.createLock(beneficiary, futureReleaseTime, { from: thirdParty })
+            );
+          });
         });
 
         describe('if owner is calling', function () {
-          beforeEach(async function () {
-            ({ logs: this.logs } = await this.builder.createLock(beneficiary, this.releaseTime, { from: owner }));
-
-            this.logs.filter(e => e.event === 'LockCreated').find(e => {
-              this.lockAddress = e.args.timelock;
-            });
-
-            this.timelock = await TokenTimelock.at(this.lockAddress);
-            await this.token.transfer(this.timelock.address, amount, { from: owner });
-          });
-
-          it('should emit a LockCreated event', async function () {
-            expectEvent.inLogs(this.logs, 'LockCreated', {
-              timelock: this.timelock.address,
-              beneficiary: beneficiary,
-              releaseTime: this.releaseTime,
-            });
-          });
-
-          context('once deployed', function () {
-            shouldBehaveLikeTokenTimelock(beneficiary, amount);
-          });
-        });
-
-        describe('if thirdParty is calling', function () {
-          it('reverts', async function () {
+          it('rejects a release time in the past', async function () {
+            const pastReleaseTime = (await time.latest()).sub(time.duration.years(1));
             await shouldFail.reverting(
-              this.builder.createLock(beneficiary, this.releaseTime, { from: thirdParty })
+              this.builder.createLock(beneficiary, pastReleaseTime, { from: owner })
             );
+          });
+
+          describe('with release time in the future', function () {
+            beforeEach(async function () {
+              this.releaseTime = (await time.latest()).add(time.duration.years(1));
+            });
+
+            describe('creating a valid timelock', function () {
+              beforeEach(async function () {
+                ({ logs: this.logs } = await this.builder.createLock(beneficiary, this.releaseTime, { from: owner }));
+
+                this.logs.filter(e => e.event === 'LockCreated').find(e => {
+                  this.lockAddress = e.args.timelock;
+                });
+
+                this.timelock = await TokenTimelock.at(this.lockAddress);
+                await this.token.transfer(this.timelock.address, amount, { from: owner });
+              });
+
+              it('should emit a LockCreated event', async function () {
+                expectEvent.inLogs(this.logs, 'LockCreated', {
+                  timelock: this.timelock.address,
+                  beneficiary: beneficiary,
+                  releaseTime: this.releaseTime,
+                });
+              });
+
+              context('once deployed', function () {
+                shouldBehaveLikeTokenTimelock(beneficiary, amount);
+              });
+            });
           });
         });
       });
